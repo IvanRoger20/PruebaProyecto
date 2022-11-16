e6c3de15223bfa783ac003ea6d07e36b99bb5f1a
invitado <invitado@LAB010209.itchii.lab>
2022-11-16 13:23:37 -0700

    Carga Inicial

---
+++ src/pruebaproyecto/PruebaProyecto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebaproyecto;
+
+/**
+ *
+ * @author invitado
+ */
+public class PruebaProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
